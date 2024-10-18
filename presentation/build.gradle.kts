05a5cf1177ba266fc47bf270afdb1a0311f6e744
nice <@https://github.com/mahmoudkarieem>
2024-10-18 17:55:10 +0300

    Initial commit

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
+    id ("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.example.presentation"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.compose.material3:material3")
+
+    implementation(project(":domain"))
+
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+
+        implementation("andrioidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("andrioidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("andrioidx.compose.runtime:runtime-livedata:1.2.0-beta02")
+    implementation("andrioidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
+
+
+    implementation ("com.google.dagger:hilt-android:2.48")
+    kapt( "com.google.dagger:hilt-compiler:2.48")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-gif:2.4.0")
+
+
+    implementation ("androidx.paging:paging-runtime:3.1.1")
+    implementation ("androidx.paging:paging-common-ktx:3.3.2")
+    implementation ("androidx.paging:paging-compose:3.2.0-rc01")
+
+
+
+
+}
